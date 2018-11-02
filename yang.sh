6bd0c0d8e46eeba203faa2e4eacb9c0c884854c5
hurted <44683565+hurted@users.noreply.github.com>
2018-11-02 13:21:12 +0700

    Create yang.sh

---
+++ yang.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/bash
+#apa lu >:[]
+
+clear
+
+echo """
+sleep 1
+echo """\033[96m
+     <={----------------------------}=>
+         Author  = 
+                  Nama:~FirziiJT
+                  Nick:~Mr.HurT3d
+                  
+         Code    = Bash
+         Gua = Noob Player
+         Date    = 10-10-100000
+     <={______________________________}=>""" |lolcat
+date | lolcat
+echo "\033[93m"
+echo "Ketik \033[96mL \033[93muntuk lanjut"
+read -p "Ketik :" L ;
+
+sleep 2
+echo "\033[93mSelamat Datang DiTool Mr.HurT3d :v)"
+echo """Tools ini Di Buat Oleh Neubi"""
+
+
+sleep 3
+echo "\033[94mdisini anda bisa mendapatkan jodoh"
+sleep 1
+echo "\033[96mpastikan penyimpanan anda cukup "
+sleep 2
+echo "\033[92mMungkin Ini memerlukan waktu yg lama"
+sleep 3
+echo "\033[93mY \033[91m[\033[93mLanjut\033[91m] \033[94m/ \033[93mN \033[91m[\033[93mTidak\033[91m] \033[93m"
+sleep 1
+fi
+read -p "[Y / N ] :" Y ;
+
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+echo "\033[96m"
+apt update&&apt upgrade -y
+pkg install git -y php -y toilet -y
+pkg install python -y python2 -y python3 -y
+pkg install curl -y
+pkg install figlet -y cowsay -y vim
+pkg install ruby -y wget gem
+pip install requests
+pip2 install mechanize
+git clone https://github.com/hatakecnk/xNot_Found
+git clone https://github.com/droidxerror123/T.HUNTERv2
+git clone https://github.com/droidxerror123/SpamISH
+git clone https://github.com/droidxerror123/T.DEF-09
+git clone https://github.com/Gameye98/santet-online
+git clone https://github.com/Ubaii/script-deface-creator
+git clone https://github.com/Gameye98/Lazymux.git
+git clone https://github.com/zerosvn/ktpkkgenerate
+git clone https://github.com/Xi4u7/A-Rat.git
+git clone https://github.com/cyweb/hammer
+git clone https://github.com/4L13199/LITEDDOS
+git clone https://github.com/UltimateHackers/ReconDog
+git clone https://github.com/Rusmana-ID/rus
+git clone https://github.com/rootM3eX/VTools
+git clone https://github.com/evait-security/weeman.git
+git clone https://github.com/T14RB373T/TelSpam
+git clone https://github.com/pirmansx/mbf
+git clone https://github.com/albertoanggi/xl-py
+git clone https://github.com/Junior60/vbug
+git clone https://github.com/Cvar1984/LiteOTP
+git clone https://github.com/LionSec/katoolin.git
+
+git clone https://github.com/grafov/hulk
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+
+git clone https://github.com/4L13199/LITESPAM
+git clone https://github.com/re4lity/PoC-Exploit
+git clone https://github.com/sqlitebrowser/sqlitebrowser
+git clone https://github.com/OffensivePython/Saddam
+git clone https://github.com/kereh/Good_Terminal
+git clone https://github.com/kereh/Deface-create
+git clone https://github.com/kereh/HN-Installer
+git clone https://github.com/Gameye98/Komodo
+git clone https://github.com/torvalds/linux
+echo """\033[91mDone.....
+\033[96mTools Telah Terinstall :) 
+\033[94mTerima Kasih Telah Menggunakan 
+\033[92mTools Ini _/\_"""
+exit
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+sh HurT3d.sh
+fi
+
+
